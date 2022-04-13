b5a38a86040adcc86ed8fc93de59bcc877dbcf93
Jun Shimura <junshimura@gmail.com>
2022-04-13 14:06:48 +0900

    プロジェクト ファイルを追加します。

---
+++ CPP2022ex02/CPP2022ex02.cpp
@@ -0,0 +1,17 @@
+﻿// CPP2022ex02.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	const int DATA_NUM = 10;	// 配列の要素数
+
+	// 10人の学生のテストの得点を格納した配列
+	int point[] = { 85, 72, 63, 45, 100, 98, 52, 88, 74, 65 };
+	int i, j;			// 配列の要素番号（ループカウンタ）
+	char grade;			// 成績の評価
+
+	return 0;
+}
